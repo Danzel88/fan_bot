03715677a1a94f0dc63efab703c9e5094f08dfb9
gecnepago@gmail.com <mpro7p332199AS88>
2021-12-29 12:23:21 +0300

    - Описать логику при нестандартном поведении пользователя / DONE.  - Получить тексты диалогов / DONE  - ограничить стейтами отправку сообщений всем юзерам от админа / DONE  - Рассылка пользователям / DONE  - проверка ручного ввода (age.isdgit, city in city_list) / DONE  -  - прием 2х фотографий только от зареганных юзеров / частично DONE

--- handlers/event_guest.py
+++ handlers/event_guest.py
@@ -114,17 +114,17 @@ async def process_city(message: types.Message, state: FSMContext):
 
 
 async def process_review(message: types.Message, state: FSMContext):
-    review_tex = message.text
+    review_text = message.text
     if message.photo:
+        print('photo eeee')
+        review_text = message.caption
         await process_photo(message, state)
-        review_tex = message.caption
-
-    await state.update_data(review=review_tex)
+    await state.update_data(review=review_text)
     await state.update_data(tg_id=message.from_user.id)
     user_data = await state.get_data()
     await message.answer(f"{msg.final_msg}")
-    await state.finish()
-    # await FaneronUsers.next()
+    # await state.finish()
+    await FaneronUsers.next()
     await db.update_user(presence=user_data["presence"], person_role=user_data["role"],
                          age=user_data["age"], city=user_data["city"],
                          review=user_data["review"], tg_id=user_data["tg_id"])
@@ -133,8 +133,33 @@ async def process_review(message: types.Message, state: FSMContext):
 async def process_photo(message: types.Message, state: FSMContext):
     photo_dir = f"{os.getcwd()}\photos"
     photo_name = f"{message.from_user.id}"
+    if await photo_counter(message.from_user.id):
+        print(f'first photo found')
+        await message.photo[-1].download(destination_file=f"{photo_dir}\{photo_name}\{photo_name}-1.jpg")
+        await message.answer(f'Второе фото получили')
+        await state.finish()
+        return
     await message.photo[-1].download(destination_file=f"{photo_dir}\{photo_name}\{photo_name}.jpg")
     # await state.finish()
+    return
+
+
+async def photo_counter(tg_id: int):
+    photo_dir = f"{os.getcwd()}\photos"
+    photo_name = f"{tg_id}"
+    exist_file = os.path.exists(f"{photo_dir}\{photo_name}\{photo_name}.jpg")
+    return exist_file
+
+#
+# async def final_update_user_data(message, state: FSMContext):
+#     await state.update_data(review=message)
+#     await state.update_data(tg_id=message.from_user.id)
+#     await state.finish()
+#     user_data = await state.get_data()
+#     print(user_data)
+#     await db.update_user(presence=user_data["presence"], person_role=user_data["role"],
+#                          age=user_data["age"], city=user_data["city"],
+#                          review=user_data["review"], tg_id=user_data["tg_id"])
 
 
 def register_faneron_users_handler(dp: Dispatcher):
@@ -144,4 +169,4 @@ def register_faneron_users_handler(dp: Dispatcher):
     dp.register_message_handler(process_age, state=FaneronUsers.waiting_for_age)
     dp.register_message_handler(process_city, state=FaneronUsers.waiting_for_city)
     dp.register_message_handler(process_review, state=FaneronUsers.waiting_review, content_types=['photo', 'text'])
-    # dp.register_message_handler(process_photo, state=FaneronUsers.waiting_photo, content_types=['photo'])
+    dp.register_message_handler(process_photo, state=FaneronUsers.waiting_photo, content_types=['photo'])
