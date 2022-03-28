9051663490ad80468d970994e3ad3888c1d2d689
gecnepago@gmail.com <mpro7p332199AS88>
2022-03-28 18:44:28 +0300

    Дописаны тудушки     - дописать обработчики всего опроса     - дописать работу с бд

--- handlers/event_guest.py
+++ handlers/event_guest.py
@@ -64,132 +64,28 @@ async def process_name(message: types.Message, state: FSMContext):
 
 async def process_done_project(message: types.Message, state: FSMContext):
     await state.update_data(done_project=message.text)
+    await state.update_data(service_list=[])
     await SurAgencyClient.next()
     await message.answer(msg.service_list, reply_markup=inline_kb)
     await message.answer("После выбора услуг нужно подтвердить ", reply_markup=inline_accept_kb)
 
 
-async def process_service_list(state: FSMContext, data: list):
-    # await SurAgencyClient.next()
-    await state.update_data(service_list=data)
-
-
 async def callback_handler(callback: types.CallbackQuery, state: FSMContext):
-    #TODO ДОписать appen к списку услуг
-    await callback.answer("Запомнил")
-    service_list = []
-    await process_service_list(state, service_list)
+    async with state.proxy() as data:
+        s = services[callback.data]
+        if s in data['service_list']:
+            await callback.answer("Уже выбрана",show_alert=True)
+        else:
+            await callback.answer("Запомнил")
+            data['service_list'].append(services[callback.data])
 
 
 async def accept_callback_handler(callback: types.CallbackQuery, state: FSMContext):
-    #TODO дописать nextstate
     await callback.answer(" ")
-    data = await state.get_data()
-    print(data['company_name'], data['position'], data['name'],
-          data['done_project'], data['service_list'])
     await SurAgencyClient.next()
-
-# async def process_presence(message: types.Message, state: FSMContext):
-#     """Получаем от юзера его статус (Подписка на новости или Оставить отзыв)"""
-#     if message.text not in presence:
-#         await message.answer(f'{msg.change_on_exists_variable}')
-#         return
-#     await state.update_data(presence=message.text)
-#     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#     for role in person_role:
-#         keyboard.add(role)
-#     if message.text == presence[1]:
-#         accept_kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#         accept_kb.add(presence[0])
-#         await db.create_or_update_user(presence=message.text, tg_id=message.from_user.id)
-#         await message.answer(
-#             f'{msg.subscribe_answer}',
-#             reply_markup=accept_kb)
-#         return
-#     await FaneronUsers.next()
-#     await db.create_or_update_user(presence=message.text, tg_id=message.from_user.id)
-#     await message.answer(f"{msg.change_role}", reply_markup=keyboard)
-#
-#
-# async def process_role(message: types.Message, state: FSMContext):
-#     """Получеам от пользователя его роль на мероприятии"""
-#     if message.text not in person_role:
-#         await message.answer(f'{msg.change_on_exists_variable}')
-#         return
-#     await state.update_data(role=message.text)
-#     await FaneronUsers.next()
-#     await message.answer(f"{msg.change_age_interval}", reply_markup=types.ReplyKeyboardRemove())
-#
-#
-# async def process_age(message: types.Message, state: FSMContext):
-#     """Получем возраст"""
-#     if message.text.isdigit():
-#         if int(message.text) > 118:
-#             await message.answer(f"Ты супер стар. Напиши реальный возраст, это важно!")
-#             return
-#         await state.update_data(age=message.text)
-#         await FaneronUsers.next()
-#         await message.answer(f"{msg.change_city}", reply_markup=types.ReplyKeyboardRemove())
-#     else:
-#
-#         await message.answer(f"{msg.wrong_format_age}")
-#
-#
-# async def process_city(message: types.Message, state: FSMContext):
-#     """Получеам город"""
-#     await state.update_data(city=message.text)
-#     await FaneronUsers.next()
-#     await message.answer(f'{msg.get_review_and_message}',
-#                          reply_markup=types.ReplyKeyboardRemove())
-#
-#
-# async def write_db_user_data(state: FSMContext):
-#     """Пишем в БД полученные данные (статус, роль, возраст, город, ревью)"""
-#     user_data = await state.get_data()
-#     await db.update_user(presence=user_data['presence'], person_role=user_data["role"],
-#                          age=user_data["age"], city=user_data["city"],
-#                          review=user_data["review"], tg_id=user_data["tg_id"])
-#
-#
-# async def process_review(message: types.Message, state: FSMContext):
-#     """Получем текст отзыва и фотографии. В Зависимости от контекста сообщения запускаем обработку медиа из сообщения"""
-#     if message.photo:
-#         if not message.caption:
-#             await asyncio.sleep(1.0)
-#             await process_photo(message, state)
-#             return
-#         else:
-#             await asyncio.sleep(1.0)
-#             await process_photo(message, state)
-#             await state.update_data(review=message.caption)
-#             await state.update_data(tg_id=message.from_user.id)
-#             await write_db_user_data(state)
-#     else:
-#         await state.update_data(review=message.text)
-#         await state.update_data(tg_id=message.from_user.id)
-#         await write_db_user_data(state)
-#     await message.answer(f"{msg.final_msg}")
-#     await FaneronUsers.next()
-#
-#
-# async def process_photo(message: types.Message, state: FSMContext):
-#     """Обработчик фотографий. Сохраняем фото с уникальным именем. Огарничение 30 фото от одного пользователя"""
-#     photo_dir = f"{os.getcwd()}/photos"
-#     photo_name = f"{message.from_user.id}"
-#     await message.photo[-1].download(destination_file=f"{photo_dir}/{photo_name}/{photo_name}_{datetime.datetime.now().time()}.jpg")
-#     if len(os.listdir(f"{photo_dir}/{photo_name}")) >= 30:
-#         await message.answer(f"{msg.photo_limit}")
-#         await state.finish()
-#         return
-#     logging.warning(f"user {message.from_user.id} add photo")
-#
-#
-# async def spam_process(message: types.Message, state: FSMContext):
-#     """Обработка сообщений отправленных после отзыва и фото... или до отзыва... или где то в середине, короче отлов
-#     спама"""
-#     await message.answer(f'{msg.spam_handler}', parse_mode="HTML")
-#     await asyncio.sleep(1.0)
-#     await message.answer(f'{msg.trouble_shutting}', parse_mode="HTML")
+    # data = await state.get_data()
+    # print(data['service_list'])
+    await callback.message.answer(msg.manager_name)
 
 
 def register_faneron_users_handler(dp: Dispatcher):
