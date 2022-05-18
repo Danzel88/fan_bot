82b7be29c845bb3536168372c4989df97f6dd8d3
Danzel88 <gecnepago@gmail.com>
2022-05-18 22:58:58 +0300

    Готова выгрузка по расписанию автоматом с разбивкой по датам #TODO     - Добить автоматическое форматирование новых страниц     - Реализовать выгрузку из БД вручную     - Рефакторинг кода database модуля и парсера БД

--- parser_db.py
+++ parser_db.py
@@ -24,11 +24,11 @@ logging.basicConfig(
 
 
 def copy_db(source, dst):
+    """Копируем БД"""
     try:
         shutil.copy(source, dst)
         logging.warning('Database copied successfully')
-        path_db = f"{os.path.isfile(f'{dst_path}faneron.db')}"
-        return path_db
+        os.path.isfile(f'{dst_path}faneron.db')
     except shutil.SameFileError:
         logging.warning("Source and destination represents the same file.")
     except PermissionError:
@@ -37,47 +37,56 @@ def copy_db(source, dst):
         os.makedirs(f'{dst}')
 
 
-def get_data_from_db(db_path, lst_id=None):
+def get_data_from_db(db_path: str, lst_id: int = None):
+    """Получаем данные из БД в формате pandas DataFrame"""
     conn = sqlite3.connect(db_path)
     if lst_id:
         df = pd.read_sql(f'select * from faneron_users where id>{lst_id}', conn)
+        conn.close()
     else:
         df = pd.read_sql('select * from faneron_users', conn)
+        conn.close()
     return df
 
 
-def db_to_excel(df):
+def df_to_excel(df):
+    """Записываем DataFrame в xlsx и возвращаем имя полученного файла"""
     df.to_excel(f'{dst_path}all_users_{datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.xlsx',
                 index=False)
-    files = sorted(os.listdir(dst_path), reverse=True)
-    return files
+    files = os.listdir(dst_path)
+    return files[-1]
 
 
-def review_process(files):
-    file = files[-1]
+def check_backup_db(db_path):
+    """Проверяем наличие бэкапа БД"""
+    return os.path.exists(db_path)
+
+
+def write_to_google_sheet(file):
+    """Запись на новый лист goolge sheet. Листу присваивается имя file
+    Имя листа можно изменить в коде writer"""
     df = pd.read_excel(f'/home/den/code/fan_bot/user_data_for_analize/{file}',)
     df = df.fillna('')
     gid_name = create_new_sheet(sheet_name=file)
     writer(values=df.values.tolist(), gid_name=gid_name)
 
 
-def main():
-    main_db_df = get_data_from_db('/home/den/code/fan_bot/databases/faneron.db')
-    backup_db_df = get_data_from_db('/home/den/code/fan_bot/user_data_for_analize/faneron.db')
-    c1 = main_db_df.shape[0]
-    c2 = backup_db_df.shape[0]
-    if c1 > c2:
-        # try:
-        df = get_data_from_db(source_db, lst_id=c2)
-        files_names = db_to_excel(df)
-
-        review_process(files_names)
-        copy_db(source_db, dst_path)
-        # except Exception as e:
-        #     print(e)
-        #     logging.error(e)
+def review_processing(main_db, backup_db):
+    m_db = get_data_from_db(main_db).shape[0]
+    b_db = get_data_from_db(backup_db).shape[0]
+    if m_db > b_db:
+        write_to_google_sheet(df_to_excel(get_data_from_db(main_db, lst_id=b_db)))
     else:
-        logging.warning(f"{datetime.datetime.now().date()} Нет новых отзывов")
+        write_to_google_sheet(df_to_excel(get_data_from_db(main_db)))
+
+
+def main():
+    if check_backup_db(f'{dst_path}faneron.db'):
+        review_processing(source_db, f'{dst_path}faneron.db')
+        copy_db(source_db, f'{dst_path}faneron.db')
+        return
+    copy_db(source_db, f'{dst_path}faneron.db')
+    review_processing(source_db, f'{dst_path}faneron.db')
 
 
 if __name__ == '__main__':
