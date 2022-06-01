76e229b0a5fc7253788a5ace807d32c0d2ded87c
Danzel88 <gecnepago@gmail.com>
2022-06-02 00:00:33 +0300

    loger edit

--- parser_db.py
+++ parser_db.py
@@ -81,7 +81,9 @@ def review_processing(main_db, backup_db):
     скрипта выгружаем все что есть в гланой и бэкапим БД"""
     try:
         m_db = get_data_from_db(main_db).shape[0]
+        print(m_db)
         b_db = get_data_from_db(backup_db).shape[0]
+        print(b_db)
         if m_db > b_db:
             write_to_google_sheet(df_to_excel(get_data_from_db(main_db, lst_id=b_db)))
             logging.warning('New review upload')
